97978e0e08b974f9c9eabaf1391016f60d8ef307
unknown <ajrgroupconnect@gmail.com>
2026-06-05 10:55:43 +0530

    working module 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,12 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { routes } from './app.routes';
+import {serverRoutes}  from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(serverRoutes),
 
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const serverRoutes: Routes = [
+  {
+    path: '',
+    component: App
+  }
+];
